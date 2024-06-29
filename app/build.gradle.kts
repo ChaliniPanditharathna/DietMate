0f50cb4f1cc3de68c3f7d0433f31bdf8c94917d7
Chalini Panditharathna <panditharathnac@student.douglascollege.ca>
2024-06-29 14:23:46 -0700

    Add fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -33,6 +36,26 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //Fragment dependencies
+    implementation("androidx.fragment:fragment:1.3.6")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    implementation("com.google.android.material:material:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.fragment:fragment:1.4.0")
+
+    //Firebase dependencies
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-auth")
+
+    // Retrofit dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/dietmate/MainActivity.java
+++ app/src/main/java/com/example/dietmate/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.dietmate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.example.dietmate.fragment.HistoryFragment;
+import com.example.dietmate.fragment.HomeFragment;
+import com.example.dietmate.fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NAVIGATION_HOME = R.id.navigation_home;
+    private static final int NAVIGATION_NOTIFICATIONS = R.id.navigation_notifications;
+    private static final int NAVIGATION_HISTORY = R.id.navigation_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                /*switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.navigation_notifications:
+                        selectedFragment = new NotificationFragment();
+                        break;
+                    case R.id.navigation_history:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                }*/
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navigation_home) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.navigation_notifications) {
+                    selectedFragment = new NotificationFragment();
+                } else if (itemId == R.id.navigation_history) {
+                    selectedFragment = new HistoryFragment();
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
 }
